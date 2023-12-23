643f378ce6917fd6fc9629cd51df7cb2e2141e5e
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2023-12-23 20:13:29 +0100

    Update files, add GetProducts and CreateProduct Unit Test

--- IoC/IoCTest/IoCTest.cs
+++ IoC/IoCTest/IoCTest.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using productMicroservice.Data;
+using productMicroservice.Data.Repository;
+using productMicroservice.Services;
 
 namespace productMicroservice.IoC.IoCTest
 {
     public static class IoCTest
     {
+        public static IServiceCollection ConfigureInjectionDependencyRepositoryTest(this IServiceCollection services)
+        {
+            services.AddScoped<ProductRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureInjectionDependencyServiceTest(this IServiceCollection services)
+        {
+            services.AddScoped<ProductService>();
+
+            return services;
+        }
         public static IServiceCollection ConfigureDBContextTest(this IServiceCollection services)
         {
             services.AddDbContext<DbContextClass>(options =>
